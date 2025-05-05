04d3d45bcb92d38911c1deac937543f7bf544c2f
Claudiazepam <martinezallendesclaudia@gmail.com>
2025-05-04 22:00:45 -0400

    revisión 20250504

---
+++ assets/js/script1.js
@@ -0,0 +1,26 @@
+console.log("Hola, estoy funcionando")
+
+const logo = document.querySelector("#identitaria");
+
+/* Intento 1
+function anadirborde(borde) {
+logo.style.borderStyle = borde;
+logo.style.borderRadius = "300px";
+logo.style.padding = "20px";
+}
+*/
+
+logo.addEventListener("click", function() {
+
+        if (logo.style.border) /*Si el borde existe*/ {
+            logo.style.border = ""; /*Lo dejo vacío*/
+        } else { /*Si no le agrego esto otro*/
+            logo.style.border = "2px solid red";
+            logo.style.borderRadius = "300px";
+            logo.style.padding = "20px";
+        }
+})
+
+
+
+
---
+++ assets/js/script3.js
@@ -0,0 +1,54 @@
+console.log("Estoy funcionando")
+
+
+
+function calcular() {
+//Seleccionar el input y su valor
+const campo1 = document.getElementById("n1");
+const d1 = (campo1.value);
+
+const campo2 = document.getElementById("n2");
+const d2 = (campo2.value);
+
+const campo3 = document.getElementById("n3");
+const d3 = (campo3.value);
+
+
+
+//Declaro los números de la contraseña
+
+const contrasena = d1+d2+d3;
+const mensaje = document.getElementById("msje")
+
+
+//Ejecuto la funcion condicional
+if (contrasena === "911") {
+    mensaje.textContent = `password 1 es correcto`;
+} else {
+    mensaje.textContent = `password incorrecto`;
+}
+
+if (contrasena === "714") {
+    mensaje.textContent = `password 2 es correcto`;
+} else {
+    mensaje.textContent = `password incorrecto`;
+}
+
+if (contrasena === "911" || "714" ) {
+    mensaje.textContent = `password correcta`;
+}
+
+}
+
+/*
+También se me ocurre que podría ser así, pero no se me ocurre cómo podría decir que debe ser true (9) true (9) true (9)
+
+if (d1 == 9) {
+d1= true;    
+} else {
+    d1 = false
+} 
+
+*/
+
+
